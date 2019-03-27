eeb3122d35c4099fdd107d513d6e0137c3f420ab
luksari <luksari23@gmail.com>
2019-03-27 11:21:34 +0100

    Added initial styles & colors

--- app/src/main/java/com/mvvm/kanban_board/MainActivity.kt
+++ app/src/main/java/com/mvvm/kanban_board/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.mvvm.kanban_board
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
