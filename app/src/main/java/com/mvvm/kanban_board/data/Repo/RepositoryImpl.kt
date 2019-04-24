2088e0c675f39612f5e237895b3a18d4223f42a8
justynias <justynakozik1@gmail.com>
2019-04-24 23:38:32 +0200

    db quaries

--- app/src/main/java/com/mvvm/kanban_board/data/Repo/RepositoryImpl.kt
+++ app/src/main/java/com/mvvm/kanban_board/data/Repo/RepositoryImpl.kt
@@ -1,9 +1,14 @@
 package com.mvvm.kanban_board.data.Repo
 
 import android.util.Log
+import androidx.annotation.RestrictTo
 import androidx.lifecycle.LiveData
 import com.mvvm.kanban_board.data.db.KanbanDao
-import com.mvvm.kanban_board.data.db.entity.User
+import com.mvvm.kanban_board.data.db.entity.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class RepositoryImpl(
     private val kanbanDao: KanbanDao
@@ -17,7 +22,11 @@ class RepositoryImpl(
         Log.d("REPO", "repo is already initialized")
     }
     init{
-        //check if the user is logged -> local db
+
+
+        //check if the user is logged in vm -> local db
         //sign in/sign up -> rest api
     }
+
+
 }
