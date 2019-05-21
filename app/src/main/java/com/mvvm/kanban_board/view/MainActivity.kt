8c664ee488db718b67f6fa336d4de9876bbd0a30
luksari <luksari23@gmail.com>
2019-05-21 12:14:28 +0200

    Added nav controller

--- app/src/main/java/com/mvvm/kanban_board/view/MainActivity.kt
+++ app/src/main/java/com/mvvm/kanban_board/view/MainActivity.kt
@@ -2,6 +2,8 @@ package com.mvvm.kanban_board.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.mvvm.kanban_board.R
 import org.koin.android.viewmodel.ext.android.viewModel
 
@@ -9,10 +11,13 @@ import org.koin.android.viewmodel.ext.android.viewModel
 class MainActivity : AppCompatActivity(){
 
     private val viewModel : MainActivityViewModel by viewModel()
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
 
         //temporary loading from json here with context
        // myRepo.initializeFakeData(this.applicationContext)
