4b96b00a1bd9c29b360dc44fd6a685926d2ea39b
luksari <luksari23@gmail.com>
2019-05-28 20:24:37 +0200

    Added navigation between Sign up and Sign In via clicking text

--- app/src/main/java/com/mvvm/kanban_board/view/sign_in/SignInFragment.kt
+++ app/src/main/java/com/mvvm/kanban_board/view/sign_in/SignInFragment.kt
@@ -1,14 +1,18 @@
 package com.mvvm.kanban_board.view.sign_in
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 
 import com.mvvm.kanban_board.R
 import com.mvvm.kanban_board.databinding.SignInFragmentBinding
+import kotlinx.android.synthetic.main.sign_in_fragment.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class SignInFragment : Fragment() {
@@ -28,8 +32,14 @@ class SignInFragment : Fragment() {
         binding.lifecycleOwner = this
         return binding.root
     }
+    private fun setupListeners(){
+        go_to_sign_up.setOnClickListener {
+            findNavController().navigate(R.id.signUpFragment)
+        }
+    }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
+        setupListeners()
         super.onActivityCreated(savedInstanceState)
     }
 
--- app/src/main/java/com/mvvm/kanban_board/view/sign_up/SignUpFragment.kt
+++ app/src/main/java/com/mvvm/kanban_board/view/sign_up/SignUpFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 
 import com.mvvm.kanban_board.R
+import com.mvvm.kanban_board.databinding.SignUpFragmentBinding
+import kotlinx.android.synthetic.main.sign_up_fragment.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class SignUpFragment : Fragment() {
@@ -21,10 +25,19 @@ class SignUpFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.sign_up_fragment, container, false)
+        val binding : SignUpFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.sign_up_fragment, container, false)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+    private fun setupListeners(){
+        go_to_sign_in.setOnClickListener {
+            findNavController().navigate(R.id.signInFragment)
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
+        setupListeners()
         super.onActivityCreated(savedInstanceState)
     }
 
