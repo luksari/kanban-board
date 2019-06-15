951d438eb6a7e9317222d29f472d3dd1aecbfe4c
luksari <luksari23@gmail.com>
2019-06-15 15:49:13 +0200

    Fixed navigation between Sign In and Board Fragment

--- app/src/main/java/com/mvvm/kanban_board/view/MainActivity.kt
+++ app/src/main/java/com/mvvm/kanban_board/view/MainActivity.kt
@@ -1,15 +1,9 @@
 package com.mvvm.kanban_board.view
 
-import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
-import android.widget.EditText
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.core.view.GravityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.OrientationHelper
-import com.google.android.material.navigation.NavigationView
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.mvvm.kanban_board.R
@@ -17,9 +11,7 @@ import com.mvvm.kanban_board.view.Board.ActivitiesAdapter
 import com.mvvm.kanban_board.view.TopBar.PagesBar.PagesAdapter
 import kotlinx.android.synthetic.main.action_bar_fragment.*
 import kotlinx.android.synthetic.main.activities_list_fragment.*
-import kotlinx.android.synthetic.main.drawer_menu_fragment.*
 import kotlinx.android.synthetic.main.pages_bar_fragment.*
-import kotlinx.android.synthetic.main.top_bar_fragment.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
 
@@ -32,36 +24,27 @@ class MainActivity : AppCompatActivity() { //, NavigationView.OnNavigationItemSe
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         navController = Navigation.findNavController(this, R.id.nav_host_fragment)
-    }
-//        setSupportActionBar(toolbar)
-//        initActionBar()
-
-
-
-        //temporary loading from json here with context
-       // myRepo.initializeFakeData(this.applicationContext)
-
-        //pagesList
-        val pages: ArrayList<String> = ArrayList()
-
-        for(i in 1..10) {
-            pages.add("Page $i")
-        }
-
-        pagesList.layoutManager = LinearLayoutManager(this, OrientationHelper.HORIZONTAL, false)
-        pagesList.adapter = PagesAdapter(pages)
-
-        //activitiesList
-        val activities: ArrayList<String> = ArrayList()
-
-        for(i in 1..10) {
-            activities.add("Do $i")
-        }
-
-        activitiesList.layoutManager = LinearLayoutManager(this)
-        activitiesList.adapter = ActivitiesAdapter(activities)
+        setSupportActionBar(toolbar)
+//        val pages: ArrayList<String> = ArrayList()
+//
+//        for(i in 1..10) {
+//            pages.add("Page $i")
+//        }
+//
+//        pagesList.layoutManager = LinearLayoutManager(this, OrientationHelper.HORIZONTAL, false)
+//        pagesList.adapter = PagesAdapter(pages)
+//
+//        val activities: ArrayList<String> = ArrayList()
+//
+//        for(i in 1..10) {
+//            activities.add("Do $i")
+//        }
+//
+//        activitiesList.layoutManager = LinearLayoutManager(this)
+//        activitiesList.adapter = ActivitiesAdapter(activities)
 
     }
+}
 
 //    //drawerMenu
 //    private fun initActionBar() {
@@ -96,4 +79,4 @@ class MainActivity : AppCompatActivity() { //, NavigationView.OnNavigationItemSe
 //        drawer_layout.closeDrawer(GravityCompat.START)
 //        return true
 //    }
-}
+
--- app/src/main/java/com/mvvm/kanban_board/view/sign_in/SignInFragment.kt
+++ app/src/main/java/com/mvvm/kanban_board/view/sign_in/SignInFragment.kt
@@ -1,6 +1,7 @@
 package com.mvvm.kanban_board.view.sign_in
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -37,9 +38,10 @@ class SignInFragment : Fragment() {
             navController.navigate(R.id.signUpFragment)
         }
         viewModel.authenticationState.observe(viewLifecycleOwner, Observer { authenticationState ->
+            // If user is authenticated succesfully, navigate to the board fragment
             if(authenticationState ==  AuthenticationState.AUTHENTICATED)
-                navController.navigate(R.id.signUpFragment) })
-                 //initially, should be boardFragment
+                navController.navigate(R.id.boardFragment)
+        })
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
