f2c7a1b0cd9dcad41a614408fa3803de9c14b423
luksari <luksari23@gmail.com>
2019-06-22 20:21:28 +0200

    deleted shit out of the project

--- app/src/main/java/com/mvvm/kanban_board/view/drawer_menu/DrawerMenuViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.mvvm.kanban_board.view.drawer_menu
-
-import androidx.lifecycle.ViewModel;
-import com.mvvm.kanban_board.data.repo.Repository
-
-class DrawerMenuViewModel(private val repository: Repository)  : ViewModel() {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/com/mvvm/kanban_board/view/drawer_menu/your_boards/YourBoardsViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.mvvm.kanban_board.view.drawer_menu.your_boards
-
-import androidx.lifecycle.ViewModel;
-import com.mvvm.kanban_board.data.repo.Repository
-
-class YourBoardsViewModel(private val repository: Repository)  : ViewModel() {
-    // TODO: Implement the ViewModel
-}
